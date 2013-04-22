28def197d6f44e2df5e5b8fc502478ca53816b57
Lars Kleen <l.kleen@bigpoint.net>
2013-04-22 14:08:18 +0200

    added fxml file

---
+++ src/main/java/org/larsworks/javafx/calculator/CalculatorController.java
@@ -0,0 +1,27 @@
+package org.larsworks.javafx.calculator;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lkleen
+ * Date: 22.04.13
+ * Time: 14:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalculatorController implements Initializable {
+
+    @FXML private Label lcd;
+    @FXML private Button btn001;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
